27ce766d176423dd2ff25d906fc638db599ea929
Rohan <pudasainirohan0@gmail.com>
2025-12-27 14:18:33 +0545

    feat: type and stuff

--- main.go
+++ main.go
@@ -6,8 +6,17 @@ import (
 	"github.com/Rohanpudasaini/learn-go/utils"
 )
 
+// var c, python, java bool
+var c, python, java = true, 12, "no!"
+
 func main() {
 	// var a, b int = 20, 0
+	i := 12
+	f := float64(i)
+	u := uint(f)
+	fmt.Println(c, python, java, i)
+	fmt.Printf("Type %T value: %v \n", f, f)
+	fmt.Println(u)
 	utils := utils.Calculator{A: 20, B: 10}
 	fmt.Println("Add: ", utils.Add())
 	fmt.Println("Subtract: ", utils.Subtract())
